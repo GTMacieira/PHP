550edad18df8d72f6fe64fcddd3e43571249f58e
GTMacieira <macieira.gtalpo@gmail.com>
2022-05-12 21:24:01 -0300

    Criando Array

---
+++ Modulo1/arrays.php
@@ -0,0 +1,9 @@
+<?php
+$ingredientes = [
+    'Pinga',
+    'limão',
+    'açucar',
+    'gelo'
+];
+echo "O primeiro ingrediente é ".$ingredientes[0]."<br/>";
+echo "O segundo ingrediente é ".$ingredientes[1];
